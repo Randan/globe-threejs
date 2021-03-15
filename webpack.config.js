866ce5cd083f580d66571502b5076f9cb3b23972
Andrew Randan <andrewrandan@gmail.com>
2021-03-16 01:29:55 +0200

    configured eslint and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: [
     require.resolve('@babel/polyfill'),
-    path.join(__dirname, '/src/index.js')
+    path.join(__dirname, '/src/index.js'),
   ],
   output: {
     path: path.join(__dirname, '/build'),
     filename: 'index-bundle.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   target: 'web',
   module: {
@@ -18,15 +18,38 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        enforce: 'pre',
+        use: ['source-map-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|woff|woff2|ttf|eot|svg)$/i,
@@ -45,13 +68,18 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, '/src/index.html'),
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'assets/images', to: 'images' },
+      ],
+    }),
   ],
   devServer: {
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     },
     port: 3000,
     historyApiFallback: true,
-  }
+  },
 };
